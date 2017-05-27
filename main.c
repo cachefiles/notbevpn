bb7f3a5e4926747d841a774aa46e472305787a77
pagxir <pagxir@gmail.com>
2017-05-27 15:32:02 +0800

    add command line support

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <assert.h>
+#include <netdb.h>
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -12,8 +13,8 @@
 #include <sys/select.h>
 
 void module_init(void);
-void * get_tcpup_data(int *len);
-void * get_tcpip_data(int *len);
+char * get_tcpup_data(int *len);
+char * get_tcpip_data(int *len);
 
 int vpn_tun_alloc(const char *dev);
 int tun_read(int fd, void *buf, size_t len);
@@ -43,11 +44,12 @@ struct icmphdr {
 
 int packet_decrypt(unsigned short key, void *dst, const void *src, size_t len)
 {
+	int i;
 	unsigned int d0 = key;
 	unsigned char *fdst = (unsigned char *)dst;
 	const unsigned char *fsrc = (const unsigned char *)src;
 
-	for (int i = 0; i < len; i++) {
+	for (i = 0; i < len; i++) {
 		*fdst++ = (*fsrc++ ^ d0);
 		d0 = (d0 * 123 + 59) & 0xffff;
 	}
@@ -57,11 +59,12 @@ int packet_decrypt(unsigned short key, void *dst, const void *src, size_t len)
 
 int packet_encrypt(unsigned short key, void *dst, const void *src, size_t len)
 {
+	int i;
 	unsigned int d0 = key;
 	unsigned char *fdst = (unsigned char *)dst;
 	const unsigned char *fsrc = (const unsigned char *)src;
 
-	for (int i = 0; i < len; i++) {
+	for (i = 0; i < len; i++) {
 		*fdst++ = (*fsrc++ ^ d0);
 		d0 = (d0 * 123 + 59) & 0xffff;
 	}
@@ -120,35 +123,152 @@ int low_link_send_data(int devfd, void *buf, size_t len, const struct sockaddr *
 	return sendto(devfd, _crypt_stream, len + sizeof(*hdr), 0, ll_addr, ll_len);
 }
 
+#define DEFAULT_TUN_NAME "tun0"
+
+static void usage(const char *prog_name)
+{
+	fprintf(stderr, "%s [options] <server>!\n", prog_name);
+	fprintf(stderr, "\t-h print this help!\n");
+	fprintf(stderr, "\t-t <tun-device> use this as tun device name, default tun0!\n");
+	fprintf(stderr, "\t-s <config-script> the path to config this interface when tun is up, default ./ifup.tun0!\n");
+	fprintf(stderr, "\t-i <interface-address> interface address, local address use for outgoing/incoming packet!\n");
+	fprintf(stderr, "\tall @address should use this format <host:port> OR <port>\n");
+	fprintf(stderr, "\n");
+
+	return;
+}
+
+int parse_sockaddr_in(struct sockaddr_in *info, const char *address)
+{
+    const char *last;
+
+#define FLAG_HAVE_DOT    1
+#define FLAG_HAVE_ALPHA  2
+#define FLAG_HAVE_NUMBER 4
+#define FLAG_HAVE_SPLIT  8
+
+    int flags = 0;
+    char host[128] = {};
+
+    info->sin_family = AF_INET;
+    info->sin_port   = htons(0);
+    info->sin_addr.s_addr = htonl(0);
+
+    for (last = address; *last; last++) {
+        if (isdigit(*last)) flags |= FLAG_HAVE_NUMBER;
+        else if (*last == ':') flags |= FLAG_HAVE_SPLIT;
+        else if (*last == '.') flags |= FLAG_HAVE_DOT;
+        else if (isalpha(*last)) flags |= FLAG_HAVE_ALPHA;
+        else { fprintf(stderr, "get target address failure!\n"); return -1;}
+    }
+
+
+    if (flags == FLAG_HAVE_NUMBER) {
+        info->sin_port = htons(atoi(address));
+        return 0;
+    }
+
+    if (flags == (FLAG_HAVE_NUMBER| FLAG_HAVE_DOT)) {
+        info->sin_addr.s_addr = inet_addr(address);
+        return 0;
+    }
+
+    struct hostent *host0 = NULL;
+    if ((flags & ~FLAG_HAVE_NUMBER) == (FLAG_HAVE_ALPHA | FLAG_HAVE_DOT)) {
+        host0 = gethostbyname(address);
+        if (host0 != NULL)
+            memcpy(&info->sin_addr, host0->h_addr, 4);
+        return 0;
+    }
+
+    if (flags & FLAG_HAVE_SPLIT) {
+        const char *split = strchr(address, ':');
+        info->sin_port = htons(atoi(split + 1));
+
+        if (strlen(address) < sizeof(host)) {
+            strncpy(host, address, sizeof(host));
+            host[split - address] = 0;
+
+            if (flags & FLAG_HAVE_ALPHA) {
+                host0 = gethostbyname(host);
+                if (host0 != NULL) memcpy(&info->sin_addr, host0->h_addr, 4);
+                return 0;
+            }
+
+            info->sin_addr.s_addr = inet_addr(host);
+        }
+    }
+
+    return 0;
+}
+
+static void run_config_script(const char *ifname, const char *script)
+{
+	char setup_cmd[8192];
+	sprintf(setup_cmd, "%s %s", script, ifname);
+	system(setup_cmd);
+	return;
+}
+
 int main(int argc, char *argv[])
 {
-	char buf[2048];
+	int i;
 	int tun, len;
+	const char *script = NULL;
+	const char *tun_name = DEFAULT_TUN_NAME;
+	char buf[2048];
 
-	int devfd, error;
+	int devfd, error, have_target = 0;
 	struct sockaddr_in ll_addr = {};
 	struct sockaddr_in so_addr = {};
 
-	tun = vpn_tun_alloc("utun0");
+	so_addr.sin_family = AF_INET;
+	so_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	for (i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-h") == 0) {
+			usage(argv[0]);
+			return 0;
+		} else if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
+			script = argv[i + 1];
+			i++;
+		} else if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
+			tun_name = argv[i + 1];
+			i++;
+		} else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+			parse_sockaddr_in(&so_addr, argv[i + 1]);
+			i++;
+		} else {
+			parse_sockaddr_in(&ll_addr, argv[i]);
+			have_target = 1;
+			continue;
+		}
+	}
+
+	if (have_target == 0 || ll_addr.sin_addr.s_addr == 0) {
+		usage(argv[0]);
+		return 0;
+	}
+
+	tun = vpn_tun_alloc(tun_name);
 	if (tun == -1) {
+		perror("vpn_tun_alloc: ");
 		return -1;
 	}
 
-	module_init();
-
-	system("ifconfig utun0 10.2.0.2/24 10.2.0.15 up");
-	system("route add -net 119.75.217.0/24 10.2.0.15");
-	system("route add -net 1.0.0.0/24 10.2.0.15");
+	int save_uid = getuid();
+	setuid(0);
+	run_config_script(tun_name, script);
 
 	devfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	assert(devfd != -1);
 
-	so_addr.sin_family = AF_INET;
-	so_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	setresuid(save_uid, save_uid, save_uid);
 	error = bind(devfd, (struct sockaddr *)&so_addr, sizeof(so_addr));
 	assert(error == 0);
 
-	so_addr.sin_addr.s_addr = inet_addr("114.215.99.157");
-	so_addr.sin_addr.s_addr = inet_addr("62.210.116.63");
+	so_addr = ll_addr;
+
+	module_init();
 
 	for (; ; ) {
 		int nready;
