6f3d7f583bd4c6fbec1b9b4081a63bfbe3656c27
pagxir <pagxir@gmail.com>
2017-09-28 02:43:23 -0400

    redirect all block tcp connection to local port to forward process

--- jni/firewall.c
+++ jni/firewall.c
@@ -59,6 +59,168 @@ int is_google_net(struct in_addr net)
 	return route_get(net) != NULL;
 }
 
+static uint16_t update_cksum(uint16_t old, int delta)
+{
+    int acc;
+
+    acc = delta;
+    acc += old;
+
+    if (acc < 0) {
+        acc  = -acc;
+        acc  = (acc >> 16) + (acc & 0xffff);
+        acc += (acc >> 16);
+        return ~acc;
+    } else {
+        acc  = (acc >> 16) + (acc & 0xffff);
+        acc += (acc >> 16);
+        return acc;
+    }
+}
+
+#define xschg(t, a, b) do { t _t = a; a = b; b = _t; } while ( 0 )
+
+static uint32_t _track_bits_1[65536 / 32] = {};
+static uint32_t _track_bits_0[65536 / 32] = {};
+static uint32_t _track_bits_2[65536 / 32] = {};
+
+static int conntrack_tcp_nat(nat_tcphdr_t *th, unsigned char *packet, size_t len)
+{
+	nat_iphdr_t * ip = (nat_iphdr_t *)packet;
+
+#if 0
+	if (ip->ip_src != inet_addr("10.24.0.1")) {
+		return 0;
+	}
+
+	if (ip->ip_dst == inet_addr("10.24.0.1")) {
+		return 0;
+	}
+#endif
+
+	uint16_t sport = htons(th->th_sport);
+	uint16_t dport = htons(th->th_dport);
+
+	if (dport > 1024) {
+		/* relay -> client */
+		assert (sport == 3080 || sport == 3443);
+		xschg(struct in_addr, ip->ip_dst, ip->ip_src);
+		th->th_sum = update_cksum(th->th_sum, th->th_sport - htons(sport - 3000));
+		th->th_sport = htons(sport - 3000);
+
+		ip->ip_sum = update_cksum(ip->ip_sum, th->th_sport - htons(sport - 3000));
+
+		uint16_t mark = th->th_dport;
+		if (th->th_flags & TH_RST) {
+			int byte_index = (mark / 32);
+			int bit_offset = (mark % 32);
+			_track_bits_0[byte_index] &= ~(1 << bit_offset);
+			_track_bits_1[byte_index] &= ~(1 << bit_offset);
+		} else if (th->th_flags & TH_FIN) {
+			int byte_index = (mark / 32);
+			int bit_offset = (mark % 32);
+			_track_bits_0[byte_index] &= ~(1 << bit_offset);
+			_track_bits_2[byte_index] |= (1 << bit_offset);
+		}
+
+		return len;
+	} else {
+		/* client -> server */
+		assert (dport == 80 || dport == 443);
+		xschg(struct in_addr, ip->ip_dst, ip->ip_src);
+		th->th_sum = update_cksum(th->th_sum, th->th_dport - htons(dport + 3000));
+		th->th_dport = htons(dport + 3000);
+
+		ip->ip_sum = update_cksum(ip->ip_sum, th->th_dport - htons(dport + 3000));
+
+		uint16_t mark = th->th_sport;
+		if (th->th_flags & TH_RST) {
+			int byte_index = (mark / 32);
+			int bit_offset = (mark % 32);
+			_track_bits_0[byte_index] &= ~(1 << bit_offset);
+			_track_bits_1[byte_index] &= ~(1 << bit_offset);
+		} else if (th->th_flags & TH_FIN) {
+			int byte_index = (mark / 32);
+			int bit_offset = (mark % 32);
+			_track_bits_1[byte_index] &= ~(1 << bit_offset);
+			_track_bits_2[byte_index] |= (1 << bit_offset);
+		}
+
+		return len;
+	}
+
+	return 0;
+}
+
+static int conntrack_tcp_marked(uint16_t mark)
+{
+	int byte_index = (mark / 32);
+	int bit_offset = (mark % 32);
+
+	if (_track_bits_0[byte_index] & (1 << bit_offset)) {
+		LOG_DEBUG("mark bits_0 failure: %d %d\n", mark, htons(mark));
+		return 0;
+	}
+
+	if (_track_bits_1[byte_index] & (1 << bit_offset)) {
+		LOG_DEBUG("mark bits_1 failure: %d %d\n", mark, htons(mark));
+		return 0;
+	}
+
+	_track_bits_0[byte_index] |= (1 << bit_offset);
+	_track_bits_1[byte_index] |= (1 << bit_offset);
+	return 1;
+}
+
+static int conntrack_tcp_lookup(nat_iphdr_t *ip, nat_tcphdr_t *th)
+{
+	uint16_t mark = th->th_sport;
+
+	int byte_index = (mark / 32);
+	int bit_offset = (mark % 32);
+
+	switch (htons(mark)) {
+		case (3000 + 80):
+			assert (htons(th->th_dport) > 1024);
+			return 1;
+
+		case (3000 + 443):
+			assert (htons(th->th_dport) > 1024);
+			return 1;
+	}
+
+	if (_track_bits_0[byte_index] & (1 << bit_offset)) {
+		return 1;
+	}
+
+	if (_track_bits_1[byte_index] & (1 << bit_offset)) {
+		return 1;
+	}
+
+	if (_track_bits_2[byte_index] & (1 << bit_offset)) {
+		_track_bits_2[byte_index] &= ~(1 << bit_offset);
+		return 1;
+	}
+
+	return 0;
+}
+
+static int conntrack_tcp_new(nat_tcphdr_t *th, unsigned char *packet, size_t len)
+{
+	int count;
+	nat_iphdr_t * ip = (nat_iphdr_t *)packet;
+	assert (htons(th->th_sport) > 1024);
+	assert (htons(th->th_dport) < 1024);
+
+	count = conntrack_tcp_nat(th, packet, len);
+
+	if (count > 0 &&
+			!conntrack_tcp_marked(th->th_sport)) {
+	}
+
+	return count;
+}
+
 int check_blocked(int tunfd, unsigned char *packet, size_t len, time_t *limited)
 {
 	ssize_t count; 
@@ -101,24 +263,39 @@ int check_blocked(int tunfd, unsigned char *packet, size_t len, time_t *limited)
 					}
 
 					time(&current);
-					if (*limited + 18 < current) {
+					if (*limited + 36 > current) {
 						LOG_DEBUG("ignore!%d tcp/%d to: :%d -> %s\n",
 								tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst));
 						break;
-					} else if (*limited + 180 > current) {
+					} else {
 						LOG_DEBUG("reject!%d tcp/%d to: :%d -> %s\n",
 								tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst));
-						count = tcp_frag_rst(th, packet);
+						count = conntrack_tcp_new(th, packet, len);
+						LOG_DEBUG("tcpnat!%d tcp/%d to: :%d -> %s, count=%d\n",
+								tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst), count);
 						if (count > 0) write(tunfd, packet, count);
-					} else {
-						LOG_DEBUG("drop!%d tcp/%d to: :%d -> %s\n",
-								tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst));
 					}
 
+					return 1;
+				} else if (conntrack_tcp_lookup(ip, th)) {
+					LOG_DEBUG("continue!%d tcp/%d to: :%d -> %s\n",
+							tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst));
+					count = conntrack_tcp_nat(th, packet, len);
+					if (count > 0) write(tunfd, packet, count);
 					return 1;
 				}
 
 			default:
+				if ((th->th_flags & (TH_SYN|TH_ACK)) == TH_SYN) {
+					LOG_DEBUG("keepalive!%d tcp/%d to: :%d -> %s\n",
+							tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst));
+				} else if (conntrack_tcp_lookup(ip, th)) {
+					LOG_DEBUG("continue!%d tcp/%d to: :%d -> %s\n",
+							tunfd, htons(th->th_dport), htons(th->th_sport), inet_ntoa(ip->ip_dst));
+					count = conntrack_tcp_nat(th, packet, len);
+					if (count > 0) write(tunfd, packet, count);
+					return 1;
+				}
 				break;
 		}
 	}
