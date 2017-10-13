31fad49d9745c23cdb2105c3a8dd428f6a7c5ffd
pagxir <pagxir@gmail.com>
2017-10-14 00:10:09 +0800

    add log api support uclibc/mingw32

--- tx_debug.c
+++ tx_debug.c
@@ -9,6 +9,10 @@
 #include <time.h>
 #include <sys/time.h>
 
+#ifdef __linux__
+#include <features.h>
+#endif
+
 #include "tx_debug.h"
 
 void __tx_check__(int cond, const char *msg, int line, const char *file)
@@ -83,7 +87,11 @@ static ssize_t _log_write(void *c, const char *buf, size_t size)
 	return size;
 }
 
-#ifdef __linux__
+#ifdef __GLIBC__
+#define _logfmt(fmt, tag) fmt
+#define log_open(cookie) fopencookie(cookie, "w+", logger_func)
+
+static int _use_line_break = 1;
 static cookie_io_functions_t logger_func = {
 	.read = NULL,
 	.write = _log_write
@@ -91,9 +99,49 @@ static cookie_io_functions_t logger_func = {
 #endif
 
 #ifdef __APPLE__
+#define _logfmt(fmt, tag) fmt
+#define log_open(cookie) fwopen(cookie, log_write)
+
+static int _use_line_break = 2;
 static int log_write(void *c, const char *buf, int size)
 {
 	return _log_write(c, buf, size);
+}
+#endif
+
+#ifndef _logfmt
+#define _logfmt(fmt, tag) _logfmt(fmt, tag)
+#define log_open(cookie) fdopen((cookie)->log_fd, "w")
+
+static int _use_line_break = 0;
+static const char *_logfmt(const char *fmt, const char *tag)
+{
+	size_t len;
+	struct timeval tv0;
+	struct tm *lt, ltv= {};
+	static char _fmt[8192];
+
+	gettimeofday(&tv0, NULL);
+	time_t now = tv0.tv_sec;
+#ifdef WINNT
+	lt = localtime(&now);
+#else
+	lt = localtime_r(&now, &ltv);
+#endif
+
+	if (*fmt == 0 || *tag == '%') {
+		return fmt;
+	}
+
+	len = snprintf(_fmt, sizeof(_fmt),
+			"%2d-%02d %02d:%02d:%02d.%03d %.1s %s\n", 1 + lt->tm_mon, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec, (int)(tv0.tv_usec / 1000), tag, fmt);
+
+	assert (len < sizeof(_fmt));
+	if (len < sizeof(_fmt) && _fmt[len - 2] == '\n') {
+		_fmt[len - 1] = 0;
+	}
+
+	return _fmt;
 }
 #endif
 
@@ -108,17 +156,11 @@ static FILE *log_get(int fd, const char *tag)
 	static struct timeval tv1;
 	static FILE *_logfp = NULL;
 
+	logcb = &_log_cookie;
+	logcb->log_fd = fd;
+
 	if (_logfp == NULL) {
-#ifdef __linux__
-		_logfp = fopencookie(&_log_cookie, "w+", logger_func);
-#endif
-#ifdef __APPLE__
-		_logfp = fwopen(&_log_cookie, log_write);
-#endif
-#ifdef WINNT
-		_logfp = stderr;
-#endif
-		assert (_logfp != NULL);
+		_logfp = log_open(&_log_cookie);
 		assert (_logfp != NULL);
 	}
 
@@ -133,13 +175,11 @@ static FILE *log_get(int fd, const char *tag)
 #endif
 	}
 
-	logcb = &_log_cookie;
 	count = snprintf(logcb->log_fmt, sizeof(logcb->log_fmt),
 			"%2d-%02d %02d:%02d:%02d.%03d %s ", 1 + lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec, (int)(tv0.tv_usec / 1000), tag);
 
 	logcb->log_len = count;
 	logcb->log_nol = 1;
-	logcb->log_fd = fd;
 
 	return _logfp;
 }
@@ -169,7 +209,7 @@ int log_tag_putlog(const char *tag, const char *fmt, ...)
 
 	if (log_logable(tag)) {
 		FILE *logfp = log_get(STDERR_FILENO, tag);
-		n = vfprintf(logfp, fmt, ap);
+		n = vfprintf(logfp, _logfmt(fmt, tag), ap);
 		log_put(logfp);
 	}
 
@@ -183,7 +223,7 @@ int log_tag_vputlog(const char *tag, const char *fmt, va_list args)
 
 	if (log_logable(tag)) {
 		FILE *logfp = log_get(STDERR_FILENO, tag);
-		n = vfprintf(logfp, fmt, args);
+		n = vfprintf(logfp, _logfmt(fmt, tag), args);
 		log_put(logfp);
 	}
 
